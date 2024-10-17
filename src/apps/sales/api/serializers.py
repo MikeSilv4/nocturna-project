3d97893faaa068b400ec97d4878a656c4836ad43
Guest1234575 <kuritavinicius@gmail.com>
2024-10-17 10:46:26 -0300

    Cofiguração de sales

--- src/apps/sales/api/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from src.apps.sales.models import Sales 
-
-class SalesAllFieldsSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Sales
-        fields = "__all__"
--- src/apps/sales/models.py
+++ src/apps/sales/models.py
@@ -1,18 +1,3 @@
 from django.db import models
 
 # Create your models here.
-
-class Sales(models.Model):
-
-    name = models.CharField("Item name", max_length=256, null=False)
-    brand = models.CharField("Item brand", max_length=128, null=True, default=None)
-    model = models.CharField("Item model", max_length=128, null=True, default=None)
-    description = models.CharField("Item description", max_length=512, null=False, default=None)
-    value = models.DecimalField("Item value", max_digits=20, decimal_places=4, null=False, default=None)
-    quantity = models.BigIntegerField("Quantity field", null=False)
-    #foregnkey
-    user = models.BigIntegerField("User ID field", null=False)
-
-    class Meta:
-        managed = True
-        db_table = "sales"
